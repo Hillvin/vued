a89557f3e41bce2b5f38b97807baa629ac615d6f
Hillvin <hillvin529@163.com>
2018-09-25 16:05:08 +0800

    2

--- readme.md
+++ readme.md
@@ -1,31 +1,14 @@
-### vuec
-1. 顶部用mint-ui即可
-2. 中间用 <router-view>占位即可
-3. 底部用MUI即可
-    注意：
-    * 底部切换高亮显示可以用 
-        利用https://router.vuejs.org/zh/installation.html网站里的router-link（API文档里） 里的active-class有默认值可以通过路由的构造选项 linkActiveClass 来全局配置
-        即 main.js 里的
-            var router1 = new vueRouter({
-                linkActiveClass:'mui-active',
-	            routes:[
-		                    {path:'/login',component:login}, 
-		                    {path:'/register',component:register}
-	                    ]
-	        });
-4.  App.vue
-    <router-link class="mui-tab-item" to="/home"></router-link>
-    <router-link class="mui-tab-item" to="/shopcar"></router-link>
-    main.js
-    import home from './components/Home.vue';
-    import shopcar from './components/shopcar/car.vue';
-    var router1 = new vueRouter({
-	    routes:[
-		    {path:'/home',component:home}, 
-		    {path:'/shopcar',component:shopcar}
-	    ]
-	});
-    components下建Home.vue
-    components下建shopcar文件夹里面建car.vue
-5. 注意 每个组件 template里的div会被header挡住一部分；每个div上加个id='tmpl' 
-    #tpml{margin-top:45px;margin-bottom: 50px;}写在site.css里
+### vued 
+ajax 获取图片 轮播图
+使用 mint-ui 里的 <mt-swipe></mt-swipe>组件 http://mint-ui.github.io/docs/#/zh-cn2/swipe
+1.  <mt-swipe :auto="4000">
+        <mt-swipe-item>1</mt-swipe-item>
+        <mt-swipe-item>2</mt-swipe-item>
+        <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
+2. vue-resource
+   npm i vue-resource --save
+3. //7.0 将vue-resource 在vue中绑定，自动在vue对象实例上注入一个$http对象就可以使用ajax方法了
+    import vueResource from 'vue-resource';
+    Vue.use(vueResource);
+4. 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,8 @@
         <!-- 轮播图 -->
         <mt-swipe :auto="1000">
             <!-- <mt-swipe-item>111111</mt-swipe-item>
-            <mt-swipe-item>222222</mt-swipe-item>
-            <mt-swipe-item>333333</mt-swipe-item> -->
+                <mt-swipe-item>222222</mt-swipe-item>
+                <mt-swipe-item>333333</mt-swipe-item> -->
             <mt-swipe-item v-for="item in list" :key="item.id">
                 <img :src="item.list" alt="" />
             </mt-swipe-item>
@@ -12,30 +12,49 @@
     </div>
 </template>
 <script>
+import { Toast } from 'mint-ui';
     export default {
-        data(){
+        data() {
             return {
-                list:[
-                    {"id":1,"img":'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1537866851923&di=cfde4a24331343f4a1a28c84b435f87a&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Fb%2F55597435bb036.jpg'},
-                    {"id":2,"img":'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1537866888415&di=7e35fd6f9bd327cf123dc35df5488a81&imgtype=jpg&src=http%3A%2F%2Fimg4.imgtn.bdimg.com%2Fit%2Fu%3D2899142879%2C4043532548%26fm%3D214%26gp%3D0.jpg'},
-                    {"id":3,"img":'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1537866852036&di=53cb60ce1a0ad6bdc05d0bf39f9dd7d5&imgtype=0&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201407%2F05%2F20140705225720_KQkMH.jpeg'}
-                ]
+                list: []
+            }
+        },
+        created() {
+            // 当页面中的 data 和 methods 对象都创建完毕以后，就会自动调用created
+            this.getimgs();
+        },
+        methods: {
+            getimgs() {
+                // 实现ajax 请求
+                // 1.0 确定url
+                var url ='http://webhm.top:8899/api/getlunbo';
+                // 2.0用$http.get()
+                this.$http.get(url).then(function(response){
+                    var data = response.body;
+                    // 错误处理 服务器返回的
+                    if(data.status!=0){
+                        Toast(data.message)
+                        return
+                    }
+                    // 如果服务器返回的数据正常则赋值给list
+                    this.list = data.message;
+
+                })
             }
         }
     }
 </script>
 <style scoped>
-.mint-swipe{
-    width:100%;
-    height:300px;
-}
-.mint-swipe-item{
-    width:100%;
-    height:300px;
-    background: red;
-}
-.mint-swipe-item img{
-    width:100%;
-
-}
+    .mint-swipe {
+        width: 100%;
+        height: 300px;
+    }
+    .mint-swipe-item {
+        width: 100%;
+        height: 300px;
+        background: red;
+    }
+    .mint-swipe-item img {
+        width: 100%;
+    }
 </style>
